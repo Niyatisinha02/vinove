0d239510975899bceadc7e76ee1cf87842d356a0
NiyatiSinha02 <niyati.kth@gmail.com>
2020-03-03 19:14:24 +0530

    added

--- src/test/java/testcases/Add_User.java
+++ src/test/java/testcases/Add_User.java
@@ -1,6 +1,7 @@
 package testcases;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
@@ -16,16 +17,25 @@ public class Add_User extends Common_Functions{
 			Wait(2000);
 			
 			Click(2, "Add User");
-			String val ="test123456789@azimuth.com";
-			TextField(4, "email", val);
-			
-			TextField(4,"lastName","sinha");
-			TextField(4, "firstName", "niyati");
-			wait(2000);
-			Click(3,"//div[@id='page-content-wrapper']/app-create-user/div/div/form/div[4]/div/button/i");
-			 Click(3," //div[@id='page-content-wrapper']/app-create-user/div/div/div/div/button[2]/i");
+			String val ="test113@azimuth.com";
+		
+		  TextField(4, "email", val);
+		 
+		 TextField(4,"lastName","sinha"); TextField(4, "firstName", "niyati");
+		 
+		 Click(3,"//button[@class=\"btn btn-round-edge btn-line green_btn ng-star-inserted\"]"); 
+		 Click(3," //div[@id='page-content-wrapper']/app-create-user/div/div/div/div/button[2]/i");
 			 TextField(3,"(//input[@type='text'])[8]",val);
-			 
+			 Wait(2000);
+			 Click(3,"//div[@id='page-content-wrapper']/app-create-user/div/div[3]/table/tbody/tr/td/label");
+			  
+		
+			 driver.findElement(By.name("lastName")).sendKeys(Keys.BACK_SPACE);
+			 driver.findElement(By.name("firstName")).sendKeys("A");
+			 TextField(4,"middleName","abc");
+             TextField(4, "employeeId", "1234");
+             TextField(4,"departmentName","IT");
+             Click(3, "//button[@class=\"btn btn-round-edge btn-line green_btn\"]");
 			
 	}
 
